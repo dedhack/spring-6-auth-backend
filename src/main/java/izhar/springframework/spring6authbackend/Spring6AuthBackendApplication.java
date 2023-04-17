1846b39bb414907a523673fecba5587fd259199f
izharj <eezhar@gmail.com>
2023-04-17 12:54:55 +0800

    setup spring for auth tutorial

---
+++ src/main/java/izhar/springframework/spring6authbackend/Spring6AuthBackendApplication.java
@@ -0,0 +1,13 @@
+package izhar.springframework.spring6authbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6AuthBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6AuthBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/izhar/springframework/spring6authbackend/Spring6AuthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package izhar.springframework.spring6authbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6AuthBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
